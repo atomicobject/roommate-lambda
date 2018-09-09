0e1ff2e1eec1d3974d1245ecc1b312a6d6c05390
Jordan Schaenzle <jschaenzle@gmail.com>
2018-09-09 14:35:57 -0400

    Initial commit. Only result of dotnet empty lamda project from template

---
+++ test/roommate-lambda.Tests/FunctionTest.fs
@@ -0,0 +1,21 @@
+namespace roommate_lambda.Tests
+
+
+open Xunit
+open Amazon.Lambda.TestUtilities
+
+open roommate_lambda
+
+
+module FunctionTest =
+    [<Fact>]
+    let ``Invoke ToUpper Lambda Function``() =
+        // Invoke the lambda function and confirm the string was upper cased.
+        let lambdaFunction = Function()
+        let context = TestLambdaContext()
+        let upperCase = lambdaFunction.FunctionHandler "hello world" context
+
+        Assert.Equal("HELLO WORLD", upperCase)
+
+    [<EntryPoint>]
+    let main _ = 0
