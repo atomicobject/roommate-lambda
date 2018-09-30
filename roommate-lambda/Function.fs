82f0642f6b65f30662f0f0d06167411f65bb51fc
John Ruble <john.ruble@atomicobject.com>
2018-09-30 18:01:30 -0400

    domain ownership verification code so google will send us notifications

--- roommate-lambda/Function.fs
+++ roommate-lambda/Function.fs
@@ -6,6 +6,7 @@ open Amazon.Lambda.APIGatewayEvents
 
 open System.Net
 
+open System
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.Json.JsonSerializer>)>]
@@ -15,13 +16,26 @@ open System.Net
 type Functions() =
 
     member __.Get (request: APIGatewayProxyRequest) (context: ILambdaContext) =
+        let verificationCode = Environment.GetEnvironmentVariable "GOOGLE_VERIFICATION_CODE"
+
         sprintf "Request: %s" request.Path
         |> context.Logger.LogLine
 
+        let htmlBody = sprintf 
+                        """
+                        <html>
+                          <head>
+                            <meta name="google-site-verification" content="%s" />
+                          </head>
+                          <body>Hello AWS Serverless (GET)</body>
+                        </html>
+                        """
+                        verificationCode
+
         APIGatewayProxyResponse(
             StatusCode = int HttpStatusCode.OK,
-            Body = "Hello AWS Serverless (GET)",
-            Headers = dict [ ("Content-Type", "text/plain") ]
+            Body = htmlBody,
+            Headers = dict [ ("Content-Type", "text/html") ]
         )
 
     member __.Post (request: APIGatewayProxyRequest) (context: ILambdaContext) =
