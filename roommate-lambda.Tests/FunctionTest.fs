d63c184ed486383332bc7985b0343299b874e791
John Ruble <john.ruble@atomicobject.com>
2018-09-30 08:34:37 -0400

    update tests and packages

--- roommate-lambda.Tests/FunctionTest.fs
+++ roommate-lambda.Tests/FunctionTest.fs
@@ -17,7 +17,14 @@ module FunctionTest =
         let response = functions.Get request context
 
         Assert.Equal(200, response.StatusCode)
-        Assert.Equal("Hello AWS Serverless", response.Body)
+        Assert.Equal("Hello AWS Serverless (GET)", response.Body)
 
-    [<EntryPoint>]
-    let main _ = 0
+    [<Fact>]
+    let ``Call HTTP POST on Root``() =
+        let functions = Functions()
+        let context = TestLambdaContext()
+        let request = APIGatewayProxyRequest()
+        let response = functions.Post request context
+
+        Assert.Equal(200, response.StatusCode)
+        Assert.Equal("Hello AWS Serverless (POST)", response.Body)
